422a77ca4090e7c8f36569efd9d19ae69f448c51
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-11-10 18:00:46 +0100

    Define routes

--- 03-frontend/angular-ecommerce/src/app/app.routes.ts
+++ 03-frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-export const routes: Routes = [];
+// Cuando el path matchea crea una nueva instancia del componente 'ProductListComponent'.
+export const routes: Routes = [
+    //Pasamos el parámetro id de la URL (Path Variable) al componente
+    {path: 'category/:id', component: ProductListComponent},
+    {path: 'category', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    // Si introducimos un path vacío redirige a /products
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    // Si introducen cualquier path diferente a los anteriores redirige a /products
+    {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
