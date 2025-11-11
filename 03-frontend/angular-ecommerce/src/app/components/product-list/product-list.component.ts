0641167e07ab742faa93dadc6fb9bbf83d1b7786
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-11-11 18:31:32 +0100

    front: add search products by keyword

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
   currentCategoryName: string = '';
+  searchMode: boolean = false;
 
   // DI
   // ActivatedRoute nos permite acceder al route parameter que nos interesa (id)
@@ -32,6 +33,24 @@ export class ProductListComponent implements OnInit {
 
   // Ejecutamos la función del servicio y nos suscribimos al Observable para activarlo y acceder a su información (JSON con los productos).
   listProducts() {
+    // Comprobamos si el param 'keyword' está
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    this._productService.searchProducts(theKeyword).subscribe(data => {
+      this.products = data;
+    })
+  }
+
+  handleListProducts() {
     // Comprueba si el param 'id' está disponible.
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
@@ -44,7 +63,7 @@ export class ProductListComponent implements OnInit {
     } else {
       // Si no tenemos category id devolvemos la categoría 1 por defecto.
       this.currentCategoryId = 1;
-      this.currentCategoryName = 'Books'
+      this.currentCategoryName = 'Books';
     }
 
     // Obtener los productos específicos a partir del id obtenido
